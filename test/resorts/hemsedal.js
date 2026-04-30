37635f4eab8c56e7feb591ede4ec84dc38661070
David Hales <david.hales@skybettingandgaming.com>
2026-04-30 16:23:03 +0100

    added skistar resorts of Are Trysil & Hemsedal

---
+++ test/resorts/hemsedal.js
@@ -0,0 +1,26 @@
+import lifts from '../lifts/index.js';
+
+lifts('hemsedal', 'html', {
+  'Hollvin Express': 'closed',
+  'Lodge Express': 'open',
+  Roniheisen: 'open',
+  Tindenheisen: 'open',
+  Skarsnuten: 'open',
+  Hamarheisen: 'closed',
+  'Totten 2': 'open',
+  'Valle 1': 'open',
+  'Valle 2': 'closed',
+  Fjellreven: 'open',
+  Fjellugla: 'closed',
+  Olaheisen: 'open',
+  'Fjellheisen 1': 'open',
+  'Fjellheisen 2': 'open',
+  Ulven: 'closed',
+  Elgen: 'closed',
+  Totteskogheisen: 'closed',
+  Jerven: 'closed',
+  Gaupa: 'closed',
+  'Lille-Olav': 'open',
+  'Fjellnest 1': 'closed',
+  'Fjellnest 2': 'closed'
+});
---
+++ test/resorts/trysil.js
@@ -0,0 +1,48 @@
+import lifts from '../lifts/index.js';
+
+lifts('trysil', 'html', {
+  // Turistsentret
+  Trysilgondolen: 'closed',
+  Liekspressen: 'closed',
+  Fjellekspressen: 'closed',
+  Knetta: 'closed',
+  Oddtrekket: 'closed',
+  Hygglo: 'closed',
+  'Hygglo 2': 'closed',
+  Fryvil: 'closed',
+  Tussi: 'closed',
+  Eventyr: 'closed',
+  'Eventyr 2': 'closed',
+  Hesten: 'closed',
+  'Rullebånd Eventyr': 'closed',
+  'Tunnelbåndet Eventyr': 'closed',
+  // Skihytta
+  'Skihytta Ekspress': 'closed',
+  'Tolver´n': 'closed',
+  Oletrekket: 'closed',
+  Valleheisen: 'closed',
+  Håvitrekket: 'closed',
+  'Rullebånd Skihytta Ekspress': 'closed',
+  'Håvi 1': 'closed',
+  'Håvi 2': 'closed',
+  // Høyfjellssentret / Fågeråsen
+  Toppekspressen: 'closed',
+  Brynbekken: 'closed',
+  'Stormyra 2': 'closed',
+  Stormyra: 'closed',
+  Familietrekket: 'closed',
+  Kanken: 'closed',
+  Skarven: 'closed',
+  Hytteheis: 'closed',
+  Stjerna: 'closed',
+  Smotten: 'closed',
+  Isiz: 'closed',
+  Myrsnipa: 'closed',
+  'Rullebånd Skistar Lodge': 'closed',
+  'Rullebånd Hytteheis 2': 'closed',
+  'Rullebånd Skiskole': 'closed',
+  'Rullebånd Barneområdet': 'closed',
+  // Høgegga
+  Høgekspressen: 'closed',
+  Høgegga: 'closed'
+});
