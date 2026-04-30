d7aa61a8cc006c449fd5d2fd24b603714d70dc0e
David Hales <david.hales@skybettingandgaming.com>
2026-04-30 16:44:31 +0100

    added bansko resort

---
+++ test/resorts/bansko.js
@@ -0,0 +1,18 @@
+import lifts from '../lifts/index.js';
+
+lifts('bansko', 'html', {
+  Cabin: 'closed',
+  Mosta: 'closed',
+  Kolarski: 'closed',
+  'Banderitsa 1': 'closed',
+  'Banderitsa 2': 'closed',
+  Shiligarnik: 'closed',
+  'Chalin Valog': 'closed',
+  Todorka: 'closed',
+  Plato: 'closed',
+  'Stara kotva': 'closed',
+  'Detska kotva': 'closed',
+  'Panitsa B': 'closed',
+  'Vlek Chalin': 'closed',
+  'Tsarna Mogila': 'closed'
+});
