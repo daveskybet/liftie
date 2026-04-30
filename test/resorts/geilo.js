1851eb1facbe059ccd05ca1722c4fb14ddd676d0
David Hales <david.hales@skybettingandgaming.com>
2026-04-30 16:37:38 +0100

    added geilo resort

---
+++ test/resorts/geilo.js
@@ -0,0 +1,24 @@
+import lifts from '../lifts/index.js';
+
+lifts('geilo', 'html', {
+  'A-Halstensgårdheisen': 'closed',
+  'B-Fugleleiken': 'closed',
+  'D-Geiloheisen ekspress': 'closed',
+  'G-Slaattaheisen': 'closed',
+  'G-Barneheisen': 'closed',
+  'G-Slaatta skibånd': 'closed',
+  'H-Korken': 'closed',
+  'K-Arnetrekket': 'closed',
+  'K-Barnetrekket': 'closed',
+  'K-Havsdalen skibånd 1': 'closed',
+  'L-Fjellheisen': 'closed',
+  'M-Vestliaheisen ekspress': 'closed',
+  'M-Bjødnaheisen': 'closed',
+  'M-Trollheisen': 'closed',
+  'N-Trollklubben': 'closed',
+  'N-Trollklubben skibånd': 'closed',
+  'P-Hufseheisen': 'closed',
+  'R-Toppheisen': 'closed',
+  'P-Kikut skibånd': 'closed',
+  'S-Kikutheisen ekspress': 'closed'
+});
